42b98426862d14eff0bb6225653b83dbf2146873
Kenan Casey <kenancasey@gmail.com>
2019-09-04 14:02:37 -0500

    Initial Commit

---
+++ Hamazon/ProfliePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Hamazon
+{
+    public partial class ProfliePage : ContentPage
+    {
+        public ProfliePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
