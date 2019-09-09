e39e8f88f939e18d27416af6277ebaa2614e42e6
Kenan Casey <kenancasey@gmail.com>
2019-09-09 13:06:49 -0500

    Layout Tweaks from Friday

--- Hamazon.iOS/AppDelegate.cs
+++ Hamazon.iOS/AppDelegate.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 
 using Foundation;
+using ImageCircle.Forms.Plugin.iOS;
 using UIKit;
 
 namespace Hamazon.iOS
@@ -23,6 +24,8 @@ namespace Hamazon.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            ImageCircleRenderer.Init();
+
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
